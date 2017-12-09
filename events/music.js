e877f3871fda571d6f8a3ee1b658b78dcb99105d
Jeffrey-Wang <17jwapples@gmail.com>
2017-12-08 19:01:51 -0500

    Reliability improvments to play; no longer gives 'already playing' error to avoid spam

--- events/music.js
+++ events/music.js
@@ -5,6 +5,7 @@ const ytdl = require('ytdl-core');
 var music = new events();
 var ytID = require("get-youtube-id")
 global.skip = false
+global.playing = false
 music.on("play", (message) =>{
   global.voteusers = []
   global.votes = 0
@@ -15,12 +16,16 @@ music.on("play", (message) =>{
 
   if(global.queue.length === 0) {
     message.channel.send({embed:utils.embed("happy", `Queue finished, leaving voice.`)})
-    message.client.voiceConnections.first().disconnect()}
-    if(global.queue.length === 0) return
-    let footer = ytID(global.queue[0]["url"])
-    if(footer === null) footer = global.queue[0]["url"]
+    message.client.voiceConnections.first().disconnect()
+    global.playing = false
+  }
+
+  if(global.queue.length === 0) return
+  let footer = ytID(global.queue[0]["url"])
+  if(footer === null) footer = global.queue[0]["url"]
 
   message.channel.send({embed:utils.embed("happy", `Now playing \`${global.queue[0]["info"]}\` queued by \`${global.queue[0]["user"].username}\` with a length of \`${global.queue[0]["minutes"]}:${global.queue[0]["seconds"]}\` `, undefined, `https://youtu.be/${footer}`)})
+  global.playing = true
   const dispatcher = message.client.voiceConnections.first().playStream(ytdl(global.queue[0]["url"], {filter: 'audioonly', quality:"lowest"}))
   dispatcher.on("end", reason => {
   console.log("neat")
@@ -49,6 +54,7 @@ module.exports.refresh = (message) => {
   global.queue = []
   global.voteusers = []
   global.votes = []
+  global.playing = false
   if(message.client.voiceConnections.first() != undefined) {
     try{music.emit("play", message)}  catch(err) {
       message.channel.send({embed:utils.embed("malfunction", `Something went wrong! \`\`\`${err}\`\`\``,"RED")})
