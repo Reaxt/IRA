46f981942697a40c30706f453b298480bcbb5923
Jeffrey-Wang <17jwapples@gmail.com>
2019-02-09 03:21:32 -0500

    videolink updates voice channel description

--- general/videolink.js
+++ general/videolink.js
@@ -7,8 +7,18 @@ module.exports = {
   	var voiceChannel = message.member.voiceChannel
     if(!voiceChannel) return message.channel.send({embed:utils.embed("sad", "You need to be in a voice channel for that.")})
 
+    // stuff for updating the url in #voice-chat description
+    var urlString = "https://discordapp.com/channels/"+message.guild.id+"/"+message.member.voiceChannelID+"/"
+    if(global.config.voice && message.channel.id == global.config.voice) {
+      var expression = new RegExp("https:\/\/discordapp\.com\/channels\/"+message.guild.id+"\/..................\/")
+      var newTopic = message.channel.topic.replace(expression, urlString)
+      message.channel.setTopic(newTopic)
+      console.log(newTopic)
+    }
+
+    // stuff for making the message to send
     var content = utils.embed(null, 
-    	"Here's your video link! \nhttps://discordapp.com/channels/"+message.guild.id+"/"+message.member.voiceChannelID+"/", 
+    	"Here's your video link! \n"+urlString, 
     	"#5c6ddb", 
     	message.member.voiceChannel.name);
     content.setTitle("🖥️ Screenshare Link");
