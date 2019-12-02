1298b847af300e05335f224d4e65001b21c42b00
Jeffrey-Wang <17jwapples@gmail.com>
2019-12-02 02:22:57 -0500

    swap images todo: color, name

--- utils/embed.js
+++ utils/embed.js
@@ -5,20 +5,30 @@ module.exports = (expression, content, color, footer) => {
 
   switch(expression) {
     case "happy":
-      url = "https://cdn.discordapp.com/attachments/203334579166117888/528414646034628625/abbyhappy.png"
+      url = "https://cdn.discordapp.com/attachments/604088023478567053/637787209721708554/happy.png"
       break;
     case "sad":
-      url = "https://cdn.discordapp.com/attachments/203334579166117888/528414649302122506/abbysad.png"
+      url = "https://cdn.discordapp.com/attachments/604088023478567053/637787206538494002/sad.png"
       break;
     case "malfunction":
-      url = "https://cdn.discordapp.com/attachments/203334579166117888/528414647573938186/abbymalfunction.png"
+      url = "https://cdn.discordapp.com/attachments/604088023478567053/637787204420108307/malfunction.png"
       Ecolor = "RED";
       break;
     case "angry":
-      url = "https://cdn.discordapp.com/attachments/203334579166117888/528414643962642465/abbyangry.png"
+      url = "https://cdn.discordapp.com/attachments/604088023478567053/637787208132198410/angry.png"
       break;
-    case "gun":
-      url = null;
+    case "abbyHappy":
+      url = "https://cdn.discordapp.com/attachments/203334579166117888/528414646034628625/abbyhappy.png"
+      break;
+    case "abbySad":
+      url = "https://cdn.discordapp.com/attachments/203334579166117888/528414649302122506/abbysad.png"
+      break;
+    case "abbyMalfunction":
+      url = "https://cdn.discordapp.com/attachments/203334579166117888/528414647573938186/abbymalfunction.png"
+      Ecolor = "RED";
+      break;
+    case "abbyAngry":
+      url = "https://cdn.discordapp.com/attachments/203334579166117888/528414643962642465/abbyangry.png"
       break;
     default:
       url = null;
@@ -27,7 +37,7 @@ module.exports = (expression, content, color, footer) => {
   if(color != undefined || color === "default") {
     Ecolor = color
   }
-  let result = new Discord.RichEmbed().setTitle("Abby").setColor(Ecolor).setThumbnail(url).setDescription(content)
+  let result = new Discord.RichEmbed().setTitle("Placeholder").setColor(Ecolor).setThumbnail(url).setDescription(content)
 
   if(footer != undefined) {
     result.setFooter(footer)
