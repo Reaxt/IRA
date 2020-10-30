5ff74448dcfb43954ee41f6614e8c89a2f1d5b43
Jeffrey-Wang <17jwapples@gmail.com>
2020-10-29 22:05:08 -0400

    update ytdl-core version

--- music/add.js
+++ music/add.js
@@ -55,80 +55,76 @@ module.exports = {
 
     //case 1: use youtube
     if (type === "youtube" || type === "search") {
-      ytdl.getBasicInfo(target, (err, info) => {
-        if(err) {
-          search(message.content.slice(5), searchopts, function(err, results) {
-            if(err) return message.channel.send({embed:utils.embed("malfunction",`something went wrong! \`\`\`${err}\`\`\``,"RED")})
-            if(results.length === 0) return message.channel.send({embed:utils.embed("malfunction", `Something went wrong! \`\`\`No results found.\`\`\``)})
-            
-            results = results.map((r) => {
-              r.title = entities.decode(r.title)
-              return r;
-            })
+      try {
+        var info = await ytdl.getBasicInfo(target)
+        var result = global.queue.filter(function( obj ) {
+          return obj.user.id == message.author.id;
+        });
+        if(result.length === 3) return message.channel.send({embed:utils.embed("sad", "Don't hog the queue, please!")})
+        let time = utils.tomins(info.player_response.videoDetails.lengthSeconds)
+        let seconds = time[1]
+        if(seconds < 10) seconds = "0" + time[1].toString()
+        global.queue.push({
+            "url":message.content.split(" ")[1], 
+            "info":info.player_response.videoDetails.title, 
+            "user":message.author, 
+            "time":info.player_response.videoDetails.lengthSeconds, 
+            "minutes":time[0], 
+            "seconds":seconds, 
+            "startTime":0, 
+            "type":"youtube"})
+        message.channel.send({embed:utils.embed("happy", `queued \`${info.player_response.videoDetails.title}\``)})
+        setTimeout(function () {
+          music.play.func(message)
+        }, 500);
+      } catch (err) {
+        search(message.content.slice(5), searchopts, function(err, results) {
+          if(err) return message.channel.send({embed:utils.embed("malfunction",`something went wrong! \`\`\`${err}\`\`\``,"RED")})
+          if(results.length === 0) return message.channel.send({embed:utils.embed("malfunction", `Something went wrong! \`\`\`No results found.\`\`\``)})
+          
+          results = results.map((r) => {
+            r.title = entities.decode(r.title)
+            return r;
+          })
 
-            message.channel.send({embed:utils.embed("happy",results.map(r => `${results.indexOf(r) + 1}  ${r.title}`))}).then(message1 => {
-              var i = 0
-              utils.numreact(message1, i, 5)
-              //2500
-  
-              const collector = message1.createReactionCollector(
-                (reaction, user) =>  numreactions.includes(reaction.emoji.name) && user.id === message.author.id,  {time:30000}
-              )
-              collector.on('collect', r => {
-                message1.delete()
-  
-  
+          message.channel.send({embed:utils.embed("happy",results.map(r => `${results.indexOf(r) + 1}  ${r.title}`))}).then(message1 => {
+            var i = 0
+            utils.numreact(message1, i, 5)
+            //2500
+
+            const collector = message1.createReactionCollector(
+              (reaction, user) =>  numreactions.includes(reaction.emoji.name) && user.id === message.author.id,  {time:30000}
+            )
+            collector.on('collect', async r => {
+              message1.delete();
+              try {
                 var result = global.queue.filter(function( obj ) {
                   return obj.user.id == message.author.id;
                 });
                 if(result.length === 10) return message.channel.send({embed:utils.embed("sad", "Don't hog the queue, please!")})
-                ytdl.getInfo(results[numreactions.indexOf(r.emoji.name)].id, (err, info) => {
-                  if(err){
-                    message.channel.send({embed:utils.embed("malfunction", `Something went wrong! \`\`\`${err}\`\`\``)})
-                  } else {
-                    let time = utils.tomins(info.length_seconds)
-                    let seconds = time[1] 
-                    if(seconds < 9) seconds = "0" + time[1].toString()
-                    global.queue.push({
-                        "url":results[numreactions.indexOf(r.emoji.name)].id, 
-                        "info":results[numreactions.indexOf(r.emoji.name)].title, 
-                        "user":message.author, 
-                        "minutes":time[0], 
-                        "seconds":seconds, 
-                        "startTime":0,
-                        "type":"youtube"})
-                    message.channel.send({embed:utils.embed("happy", `queued \`${results[numreactions.indexOf(r.emoji.name)].title}\``)})
-                    setTimeout(function () {
-                      music.play.func(message)
-                    }, 500);
-                  }
-                })
-              })
+                var info = await ytdl.getInfo(results[numreactions.indexOf(r.emoji.name)].id);
+                let time = utils.tomins(info.player_response.videoDetails.lengthSeconds)
+                let seconds = time[1] 
+                if(seconds < 9) seconds = "0" + time[1].toString()
+                global.queue.push({
+                    "url":results[numreactions.indexOf(r.emoji.name)].id, 
+                    "info":results[numreactions.indexOf(r.emoji.name)].title, 
+                    "user":message.author, 
+                    "minutes":time[0], 
+                    "seconds":seconds, 
+                    "startTime":0,
+                    "type":"youtube"})
+                message.channel.send({embed:utils.embed("happy", `queued \`${results[numreactions.indexOf(r.emoji.name)].title}\``)})
+                setTimeout(function () {
+                  music.play.func(message)
+                }, 500);
+              } catch (err) {
+                message.channel.send({embed:utils.embed("malfunction", `Something went wrong! \`\`\`${err}\`\`\``)});
+              }
             })
           })
-        } else {
-          var result = global.queue.filter(function( obj ) {
-            return obj.user.id == message.author.id;
-          });
-          if(result.length === 3) return message.channel.send({embed:utils.embed("sad", "Don't hog the queue, please!")})
-          let time = utils.tomins(info.length_seconds)
-          let seconds = time[1]
-          if(seconds < 10) seconds = "0" + time[1].toString()
-          global.queue.push({
-              "url":message.content.split(" ")[1], 
-              "info":info.title, 
-              "user":message.author, 
-              "time":info.length_seconds, 
-              "minutes":time[0], 
-              "seconds":seconds, 
-              "startTime":0, 
-              "type":"youtube"})
-          message.channel.send({embed:utils.embed("happy", `queued \`${info.title}\``)})
-          setTimeout(function () {
-            music.play.func(message)
-          }, 500);
-        }
-      })
+        })
+      }
     } // case 2: soundcloud
     else if (type == "soundcloud") {
       request(`http://api.soundcloud.com/resolve?url=${target}&client_id=${config.scid}`, (error, response, body) => {
