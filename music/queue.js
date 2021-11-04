900243be3b6372b1a0af9c2f117a18017dfca882
Wallemations <pannone292@gmail.com>
2021-11-03 20:04:00 -0400

    makes it even easier to see

--- music/queue.js
+++ music/queue.js
@@ -8,7 +8,7 @@ module.exports = {
     if(global.queue.length === 0) return message.channel.send({embed:utils.embed("sad","THERE'S NOTHING TO PLAY ADD SOMETHING WITH `command[!add]`")})
     aamessage = []
     global.queue.forEach(function(element) {
-    aamessage.push(`\`${global.queue.indexOf(element)}) ${element["info"]}\` QUEUED BY \`${element["user"].username}\` \n`)
+    aamessage.push(`${global.queue.indexOf(element)}-- \`${element["info"]}\` QUEUED BY \`${element["user"].username}\` \n`)
     })
     message.channel.send({embed:utils.embed("happy",(aamessage.join("")))})
 
