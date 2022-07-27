7279401904ce33bf5981cbfae53f00565f4595b0
Wallemations <66052067+Wallemations@users.noreply.github.com>
2022-07-27 00:14:26 -0400

    Impressive... let's see Paul Allen's debt

--- general/debt.js
+++ general/debt.js
@@ -10,8 +10,6 @@ module.exports = {
       var db = new NeDB({filename: './userdata.db'})
     	let user = message.author;
       if (message.mentions.members.first()) user = message.mentions.members.first() 
-      let total_debt = 0
-      let total_gains = 0
       try{
         db.loadDatabase(function (err) {
             if (err) {
@@ -19,15 +17,20 @@ module.exports = {
             }
         });
         db.find({}, (err, docs) => {
+          let total_debt = 0
+          let total_gains = 0
+          let count = 0
           for (let docKey in docs) {
             let doc = docs[docKey]
             total_debt += doc.debt
             total_gains += doc.coins
+            if(doc.debt > 0){
+            count++}
           }
+          message.channel.send({embed: utils.embed("happy", `YOU GUYS TOOK \`${total_debt}\` COINS FROM ME AND ONLY HAVE \`${total_gains}\` COINS LEFT MEANING THE SERVER'S NET WORTH IS \`${total_gains - total_debt}\`\n\`------------------------\`\n\`${count}\` DEBTORS OWE ME MONEY MAKING THE AVERAGE GAINS \`${total_gains / count}\` AND THE AVERAGE DEBT \`${total_debt / count}\` AND THE AVERAGE NET WORTH \`${(total_gains - total_debt) / count}\``)})
       })
-      message.channel.send({embed: utils.embed("happy", `YOU GUYS TOOK \`${total_debt}\` COINS FROM ME AND ONLY HAVE \`${total_gains}\` COINS LEFT MEANING THE SERVER'S NET WORTH IS \`${total_gains - total_debt}\``)});
     } catch(err) {
-      sentMsg.edit({embed:utils.embed(`malfunction`,`OH THAT'S NOT GOOD \`\`\`${err}\`\`\``, "RED")})
+      message.channel.send({embed:utils.embed(`malfunction`,`OH THAT'S NOT GOOD \`\`\`${err}\`\`\``, "RED")})
       }
   }
 }
