0017454726cf1c1a938c47961825b0ba5d42a83a
Wallemations <66052067+Wallemations@users.noreply.github.com>
2023-01-29 19:12:01 -0500

    progress halted

--- mod/updatecard.js
+++ mod/updatecard.js
@@ -1,20 +1,22 @@
 const Discord = require("discord.js")
-var utils = require("../utils/index.js")
+const utils = require("../utils/index.js")
 const NeDB = require('nedb')
 var confirmEmote = "✅"
 var cancelEmote = "❌"
+const Sentencer = require("sentencer")
 
 module.exports = {
-  name:"!cardClean",
-  desc:"Cleans a specific card out of everyone's inventory. Usage: !cardClean <cardName>",
+  name:"!updateCard",
+  desc:"Updates a specific card in everyone's inventories using values from cards.json. Usage: !updateCard <cardName>",
   mod:true,
   func:async function(message){
     let things = message.content.split(" ")
-    // things[1]: the name of the card
-    if(!things[1]) return(message.channel.send({embed:utils.embed("malfunction", "`Usage: !cardClean <cardName>`")}))
+    // things[1]: the name of the card (not displayName)
+    if(!things[1]) return(message.channel.send({embed:utils.embed("malfunction", "`Usage: !updateCard <cardName>. (Do not use displayName)`")}))
     var db = new NeDB({filename: './carddata.db'})
     var archdb = new NeDB({filename: './carddata_archive.db'})
-    let sentMsg = await message.channel.send({embed:utils.embed("sad", `YOU SURE YOU WANNA DELETE \`${things[1]}\` FROM EVERYONE'S INVENTORIES? YOU CAN'T UNDO THIS. WE RECOMMEND MAKING AN ANNOUNCEMENT THAT THIS HAS BEEN DONE.`)});
+    var refCards = JSON.parse(fs.readFileSync("./events/cards.json"))
+    let sentMsg = await message.channel.send({embed:utils.embed("sad", `YOU SURE YOU WANNA REWRITE \`${things[1]}\` IN EVERYONE'S INVENTORIES? THIS WILL REROLL THEIR ADJECTIVES. YOU CAN'T UNDO THIS. WE RECOMMEND MAKING AN ANNOUNCEMENT THAT THIS HAS BEEN DONE.`)});
     sentMsg.react(confirmEmote)
     sentMsg.react(cancelEmote)
     let collector = sentMsg.createReactionCollector(
@@ -35,6 +37,16 @@ module.exports = {
                         console.log("Failed to load archdata! Err: " + err);
                     }
                 });
+                let ourCard = null
+                refCards.find({}, (err, refs) => {
+                    for (let refKey in refs) {
+                        let ref = refs[refKey]
+                        if (ref.name === things[1]) {
+                            ourCard = ref
+                            break
+                        }
+                    }
+                });
                 db.find({}, (err, docs) => {
                     let count = 0
                     for (let docKey in docs) {
@@ -42,7 +54,12 @@ module.exports = {
                         if (doc.name === things[1]) {
                             count++
                             archdb.insert(doc, function(err) {if (err) console.log(err)})
-                            db.remove({_id:doc._id});
+                            doc.displayName = ourCard.displayName
+                            doc.imgURL = ourCard.imgURL
+	                        doc.series = ourCard.series
+                            doc.event = ourCard.event
+                            doc.charOwner = ourCard.charOwner
+                            doc.rarity = ourCard.rarity
                         }
                     }
                     sentMsg.edit({embed:utils.embed(`happy`, `\`parsed ${docs.length} cards and removed ${count}\``)})
