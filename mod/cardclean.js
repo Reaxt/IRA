ec6a88c1be6de3bb21899dc61d59a9a02dfa209e
Wallemations <66052067+Wallemations@users.noreply.github.com>
2023-04-07 23:12:04 -0400

    I hate javascript after this

--- mod/cardclean.js
+++ mod/cardclean.js
@@ -1,60 +1,113 @@
 const Discord = require("discord.js")
-var utils = require("../utils/index.js")
+const utils = require("../utils/index.js")
 const NeDB = require('nedb')
-var confirmEmote = "✅"
-var cancelEmote = "❌"
+const fs = require("fs");
+
+const cards = JSON.parse(fs.readFileSync("./events/cards.json"))
+const ITEMS_PER_PAGE = 10
 
 module.exports = {
-  name:"!cardClean",
-  desc:"Cleans a specific card out of everyone's inventory. Usage: !cardClean <cardName>",
-  mod:true,
-  func:async function(message){
-    let things = message.content.split(" ")
-    // things[1]: the name of the card
-    if(!things[1]) return(message.channel.send({embed:utils.embed("malfunction", "`Usage: !cardClean <cardName>`")}))
+  name: "!cardClean",
+  desc: "Deletes a specific card from everyone's inventories.",
+  mod: true,
+  func: async function (message) {
+    const cardPages = Math.ceil(cards.length / ITEMS_PER_PAGE)
+    let currentPage = 0
+
+    const embed = await generateCardListEmbed(cards, currentPage, ITEMS_PER_PAGE)
+    const sentMsg = await message.channel.send(embed)
+
     var db = new NeDB({filename: './carddata.db'})
-    var archdb = new NeDB({filename: './carddata_archive.db'})
-    let sentMsg = await message.channel.send({embed:utils.embed("sad", `YOU SURE YOU WANNA DELETE \`${things[1]}\` FROM EVERYONE'S INVENTORIES? YOU CAN'T UNDO THIS. WE RECOMMEND MAKING AN ANNOUNCEMENT THAT THIS HAS BEEN DONE.`)});
-    sentMsg.react(confirmEmote)
-    sentMsg.react(cancelEmote)
-    let collector = sentMsg.createReactionCollector(
-		(reaction, user) => (reaction.emoji.name == confirmEmote || reaction.emoji.name == cancelEmote) && user.id === message.author.id, {time:30000}
-	)
-	collector.on('collect', r=> {
-		if (r.emoji.name == confirmEmote) {
-			collector.stop("accept")
-			sentMsg.reactions.removeAll()
-			try{
-                db.loadDatabase(function (err) {
-                    if (err) {
-                        console.log("Failed to load carddata! Err: " + err);
-                    }
-                });
-                archdb.loadDatabase(function (err) {
-                    if (err) {
-                        console.log("Failed to load archdata! Err: " + err);
-                    }
-                });
-                db.find({}, (err, docs) => {
-                    let count = 0
-                    for (let docKey in docs) {
-                        let doc = docs[docKey]
-                        if (doc.name === things[1]) {
-                            count++
-                            archdb.insert(doc, function(err) {if (err) console.log(err)})
-                            db.remove({_id:doc._id});
-                        }
-                    }
-                    sentMsg.edit({embed:utils.embed(`happy`, `\`parsed ${docs.length} cards and removed ${count}\``)})
-                })
-            } catch(err) {
-                sentMsg.edit({embed:utils.embed(`malfunction`,`OH THAT'S NOT GOOD \`\`\`${err}\`\`\``, "RED")})
-                }
-        } else if (r.emoji.name == cancelEmote) {
-            collector.stop()
-			sentMsg.reactions.removeAll()
-			sentMsg.edit({embed:utils.embed(`happy`,`\`${things[1]} deletion cancelled.\``)})
-		}
-    })
+    if (cardPages > 1) {
+      await sentMsg.react("⬅️")
+      await sentMsg.react("➡️")
     }
+
+    const filter = (reaction, user) => ["⬅️", "➡️"].includes(reaction.emoji.name) && user.id === message.author.id
+    const collector = sentMsg.createReactionCollector(filter, { time: 60000 })
+    collector.on("collect", async (reaction, user) => {
+      await reaction.users.remove(user.id)
+      switch (reaction.emoji.name) {
+        case "⬅️":
+          currentPage = currentPage > 0 ? currentPage - 1 : cardPages - 1
+          break
+        case "➡️":
+          currentPage = currentPage < cardPages - 1 ? currentPage + 1 : 0
+          break
+      }
+      const newEmbed = await generateCardListEmbed(cards, currentPage, ITEMS_PER_PAGE)
+      await sentMsg.edit(newEmbed)
+    })
+
+    const messageFilter = (response) => {
+      return !isNaN(response.content) && Number(response.content) > 0 && Number(response.content) <= cards.length
+    }
+
+    try {
+        const collected = await message.channel.awaitMessages(messageFilter, { max: 1, time: 60000, errors: ["time"] });
+        const selectedCardIndex = Number(collected.first().content) - 1;
+        const selectedCard = cards[selectedCardIndex];
+        await sentMsg.reactions.removeAll();
+        db.loadDatabase(function (err) {
+          if (err) {
+            console.log("Failed to load carddata! Err: " + err);
+          }
+        });
+        sentMsg.edit({ embed: utils.embed(`happy`, `YOU SELECTED A CARD WITH INDEX \`${selectedCardIndex + 1}\`: \`${selectedCard.displayName}\` - ARE YOU SURE YOU WANT ME TO DESTROY ALL COPIES OF THIS CARD`) });
+        // Confirmation step
+        await sentMsg.react("✅");
+        await sentMsg.react("❌");
+        const confirmationFilter = (reaction, user) => ["✅", "❌"].includes(reaction.emoji.name) && user.id === message.author.id;
+        const confirmationCollector = sentMsg.createReactionCollector(confirmationFilter, { max: 1, time: 60000 });
+        confirmationCollector.on("collect", async (reaction, user) => {
+          await reaction.users.remove(user.id);
+          if (reaction.emoji.name === "✅") {
+            try {
+              // Delete all instances of the selected card in the database
+              db.remove({ name: selectedCard.name }, { multi: true }, function (err, numReplaced) {
+                if (err) {
+                  sentMsg.edit({ embed: utils.embed(`malfunction`, `AN ERROR OCCURRED WHILE DELETING THE CARD: \`\`\`${err}\`\`\``, "RED") });
+                } else {
+                  sentMsg.edit({ embed: utils.embed(`happy`, `DELETED \`${numReplaced}\` COPIES OF THE \`${selectedCard.displayName}\` CARD`) });
+                  db.persistence.compactDatafile()
+                  sentMsg.reactions.removeAll();
+                }
+              });
+            } catch (err) {
+              sentMsg.edit({ embed: utils.embed(`malfunction`, `AN ERROR OCCURRED WHILE DELETING THE CARD: \`\`\`${err}\`\`\``, "RED") });
+            }
+          } else {
+            sentMsg.edit({ embed: utils.embed(`angry`, `DELETION CANCELLED`) });
+          }
+        });
+      } catch (err) {
+        if (err instanceof Discord.Collection) {
+          sentMsg.edit({ embed: utils.embed(`angry`, `REQUEST EXPIRED`), content: null });
+        } else {
+          sentMsg.edit({ embed: utils.embed(`malfunction`, `OH THAT'S NOT GOOD \`\`\`${err}\`\`\``, "RED") });
+        }
+        await sentMsg.reactions.removeAll();
+      }
+  }
+}
+
+// Makes the list of all cards
+async function generateCardListEmbed(cards, currentPage, itemsPerPage) {
+  const startIndex = currentPage * itemsPerPage
+  const endIndex = startIndex + itemsPerPage
+  const displayCards = cards.slice(startIndex, endIndex)
+
+  
+  const embed = new Discord.MessageEmbed()
+    .setAuthor(`Card List`, "https://cdn.discordapp.com/attachments/512493868839731201/905100842460401724/sandalidle2x.gif")
+	.setDescription("`Enter the number of a card to select it.`")
+    .setColor("#f759e8")
+    .setFooter(`Page ${(startIndex/ITEMS_PER_PAGE)+1}/${Math.ceil((cards.length/ITEMS_PER_PAGE))}`)
+
+  for (let i = 0; i < displayCards.length; i++) {
+    const card = displayCards[i]
+    embed.addField(`[${startIndex + i + 1}] ${card.displayName}`, `name: \`${card.name}\``)
+  }
+
+  return embed
 }
--- mod/updatecard.js
+++ mod/updatecard.js
@@ -1,77 +1,113 @@
 const Discord = require("discord.js")
 const utils = require("../utils/index.js")
 const NeDB = require('nedb')
-var confirmEmote = "✅"
-var cancelEmote = "❌"
-const Sentencer = require("sentencer")
+const fs = require("fs");
+
+const cards = JSON.parse(fs.readFileSync("./events/cards.json"))
+const ITEMS_PER_PAGE = 10
 
 module.exports = {
-  name:"!updateCard",
-  desc:"Updates a specific card in everyone's inventories using values from cards.json. Usage: !updateCard <cardName>",
-  mod:true,
-  func:async function(message){
-    let things = message.content.split(" ")
-    // things[1]: the name of the card (not displayName)
-    if(!things[1]) return(message.channel.send({embed:utils.embed("malfunction", "`Usage: !updateCard <cardName>. (Do not use displayName)`")}))
+  name: "!updateCard",
+  desc: "Updates a specific card in everyone's inventories in relation to its status in `cards.json`. WHEN EDITING CARDS, DO NOT EDIT THE [name] SECTION.",
+  mod: true,
+  func: async function (message) {
+    const cardPages = Math.ceil(cards.length / ITEMS_PER_PAGE)
+    let currentPage = 0
+
+    const embed = await generateCardListEmbed(cards, currentPage, ITEMS_PER_PAGE)
+    const sentMsg = await message.channel.send(embed)
+
     var db = new NeDB({filename: './carddata.db'})
-    var archdb = new NeDB({filename: './carddata_archive.db'})
-    var refCards = JSON.parse(fs.readFileSync("./events/cards.json"))
-    let sentMsg = await message.channel.send({embed:utils.embed("sad", `YOU SURE YOU WANNA REWRITE \`${things[1]}\` IN EVERYONE'S INVENTORIES? THIS WILL REROLL THEIR ADJECTIVES. YOU CAN'T UNDO THIS. WE RECOMMEND MAKING AN ANNOUNCEMENT THAT THIS HAS BEEN DONE.`)});
-    sentMsg.react(confirmEmote)
-    sentMsg.react(cancelEmote)
-    let collector = sentMsg.createReactionCollector(
-		(reaction, user) => (reaction.emoji.name == confirmEmote || reaction.emoji.name == cancelEmote) && user.id === message.author.id, {time:30000}
-	)
-	collector.on('collect', r=> {
-		if (r.emoji.name == confirmEmote) {
-			collector.stop("accept")
-			sentMsg.reactions.removeAll()
-			try{
-                db.loadDatabase(function (err) {
-                    if (err) {
-                        console.log("Failed to load carddata! Err: " + err);
-                    }
-                });
-                archdb.loadDatabase(function (err) {
-                    if (err) {
-                        console.log("Failed to load archdata! Err: " + err);
-                    }
-                });
-                let ourCard = null
-                refCards.find({}, (err, refs) => {
-                    for (let refKey in refs) {
-                        let ref = refs[refKey]
-                        if (ref.name === things[1]) {
-                            ourCard = ref
-                            break
-                        }
-                    }
-                });
-                db.find({}, (err, docs) => {
-                    let count = 0
-                    for (let docKey in docs) {
-                        let doc = docs[docKey]
-                        if (doc.name === things[1]) {
-                            count++
-                            archdb.insert(doc, function(err) {if (err) console.log(err)})
-                            doc.displayName = ourCard.displayName
-                            doc.imgURL = ourCard.imgURL
-	                        doc.series = ourCard.series
-                            doc.event = ourCard.event
-                            doc.charOwner = ourCard.charOwner
-                            doc.rarity = ourCard.rarity
-                        }
-                    }
-                    sentMsg.edit({embed:utils.embed(`happy`, `\`parsed ${docs.length} cards and removed ${count}\``)})
-                })
-            } catch(err) {
-                sentMsg.edit({embed:utils.embed(`malfunction`,`OH THAT'S NOT GOOD \`\`\`${err}\`\`\``, "RED")})
-                }
-        } else if (r.emoji.name == cancelEmote) {
-            collector.stop()
-			sentMsg.reactions.removeAll()
-			sentMsg.edit({embed:utils.embed(`happy`,`\`${things[1]} deletion cancelled.\``)})
-		}
-    })
+    if (cardPages > 1) {
+      await sentMsg.react("⬅️")
+      await sentMsg.react("➡️")
     }
+
+    const filter = (reaction, user) => ["⬅️", "➡️"].includes(reaction.emoji.name) && user.id === message.author.id
+    const collector = sentMsg.createReactionCollector(filter, { time: 60000 })
+    collector.on("collect", async (reaction, user) => {
+      await reaction.users.remove(user.id)
+      switch (reaction.emoji.name) {
+        case "⬅️":
+          currentPage = currentPage > 0 ? currentPage - 1 : cardPages - 1
+          break
+        case "➡️":
+          currentPage = currentPage < cardPages - 1 ? currentPage + 1 : 0
+          break
+      }
+      const newEmbed = await generateCardListEmbed(cards, currentPage, ITEMS_PER_PAGE)
+      await sentMsg.edit(newEmbed)
+    })
+
+    const messageFilter = (response) => {
+      return !isNaN(response.content) && Number(response.content) > 0 && Number(response.content) <= cards.length
+    }
+
+    try {
+        const collected = await message.channel.awaitMessages(messageFilter, { max: 1, time: 60000, errors: ["time"] });
+        const selectedCardIndex = Number(collected.first().content) - 1;
+        const selectedCard = cards[selectedCardIndex];
+        await sentMsg.reactions.removeAll();
+        db.loadDatabase(function (err) {
+          if (err) {
+            console.log("Failed to load carddata! Err: " + err);
+          }
+        });
+        sentMsg.edit({ embed: utils.embed(`happy`, `YOU SELECTED A CARD WITH INDEX \`${selectedCardIndex + 1}\`: \`${selectedCard.displayName}\` - ARE YOU SURE YOU WANT TO UPDATE IT FOR EVERYONE`) });
+        // Confirmation step
+        await sentMsg.react("✅");
+        await sentMsg.react("❌");
+        const confirmationFilter = (reaction, user) => ["✅", "❌"].includes(reaction.emoji.name) && user.id === message.author.id;
+        const confirmationCollector = sentMsg.createReactionCollector(confirmationFilter, { max: 1, time: 60000 });
+        confirmationCollector.on("collect", async (reaction, user) => {
+          await reaction.users.remove(user.id);
+          if (reaction.emoji.name === "✅") {
+            try {
+              // Update all instances of the selected card in the database
+              db.update({ name: selectedCard.name }, { $set: { displayName: selectedCard.displayName, image: selectedCard.image, description: selectedCard.description, rarity: selectedCard.rarity } }, { multi: true }, function (err, numReplaced) {
+                if (err) {
+                  sentMsg.edit({ embed: utils.embed(`malfunction`, `AN ERROR OCCURRED WHILE UPDATING THE CARD: \`\`\`${err}\`\`\``, "RED") })
+                } else {
+                  sentMsg.edit({ embed: utils.embed(`happy`, `UPDATED \`${numReplaced}\` CARDS WITH THE NEW INFO FOR \`${selectedCard.displayName}\``) })
+                  db.persistence.compactDatafile()
+                  sentMsg.reactions.removeAll();
+                }
+              });
+            } catch (err) {
+              sentMsg.edit({ embed: utils.embed(`malfunction`, `AN ERROR OCCURRED WHILE DELETING THE CARD: \`\`\`${err}\`\`\``, "RED") });
+            }
+          } else {
+            sentMsg.edit({ embed: utils.embed(`sad`, `OK THANKS FOR WASTING MY TIME`) });
+          }
+        });
+      } catch (err) {
+        if (err instanceof Discord.Collection) {
+          sentMsg.edit({ embed: utils.embed(`angry`, `REQUEST EXPIRED`), content: null });
+        } else {
+          sentMsg.edit({ embed: utils.embed(`malfunction`, `OH THAT'S NOT GOOD \`\`\`${err}\`\`\``, "RED") });
+        }
+        await sentMsg.reactions.removeAll();
+      }
+  }
+}
+
+// Makes the list of all cards
+async function generateCardListEmbed(cards, currentPage, itemsPerPage) {
+  const startIndex = currentPage * itemsPerPage
+  const endIndex = startIndex + itemsPerPage
+  const displayCards = cards.slice(startIndex, endIndex)
+
+  
+  const embed = new Discord.MessageEmbed()
+    .setAuthor(`Card List`, "https://cdn.discordapp.com/attachments/512493868839731201/905100842460401724/sandalidle2x.gif")
+	.setDescription("`Enter the number of a card to select it.`")
+    .setColor("#f759e8")
+    .setFooter(`Page ${(startIndex/ITEMS_PER_PAGE)+1}/${Math.ceil((cards.length/ITEMS_PER_PAGE))}`)
+
+  for (let i = 0; i < displayCards.length; i++) {
+    const card = displayCards[i]
+    embed.addField(`[${startIndex + i + 1}] ${card.displayName}`, `name: \`${card.name}\``)
+  }
+
+  return embed
 }
